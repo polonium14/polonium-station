571194272d64819fdca59b4ca6a7febef50352da
nikitosych <boriszyn@gmail.com>
2025-07-11 20:48:10 +0200

    Refactor workflow to run script for checklist updates and remove inline mode logic

---
+++ .github/update-translate-checklist/run.sh
@@ -0,0 +1,11 @@
+if [ github.event.action == 'closed' ]; then
+  if [ github.event.pull_request.merged == true ]; then
+    MODE=merge
+  else MODE=nomerge
+  fi
+else MODE=open
+fi
+
+echo "Running update-translate-checklist with mode: $MODE"
+
+dotnet run --project ./pr-open/update-translate-checklist.csproj -- $MODE
