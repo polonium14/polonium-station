c9807b2495ceb9a6ec953f1e8e70a66697f0aa47
nikitosych <boriszyn@gmail.com>
2025-08-11 13:30:10 +0200

    Dodano rejestrowanie wersji silnika w funkcji get_engine_version

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -87,6 +87,7 @@ def get_engine_version() -> str:
     proc = subprocess.run(["git", "describe","--tags", "--abbrev=0"], stdout=subprocess.PIPE, cwd="RobustToolbox", check=True, encoding="UTF-8")
     tag = proc.stdout.strip()
     assert tag.startswith("v")
+    print(f"Engine version: {tag[1:]}")
     return tag[1:] # Cut off v prefix.
 
 
