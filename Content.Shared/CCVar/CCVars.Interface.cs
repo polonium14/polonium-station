1cfb9717d949a906b463ded07eb93a4e3024199b
Nikita (Nick) <174215049+nikitosych@users.noreply.github.com>
2025-09-03 17:21:10 +0200

    uuid show

--- Content.Shared/CCVar/CCVars.Interface.cs
+++ Content.Shared/CCVar/CCVars.Interface.cs
@@ -37,6 +37,10 @@ public sealed partial class CCVars
     public static readonly CVarDef<bool> OutlineEnabled =
         CVarDef.Create("outline.enabled", true, CVar.CLIENTONLY);
 
+    // Polonium
+    public static readonly CVarDef<bool> ShowUUIDButton =
+        CVarDef.Create("ui.uuid_banner_enabled", true, CVar.SERVER);
+
     /// <summary>
     /// Determines how antagonist status/roletype is displayed. Based on AdminOverlayAntagFormats enum
     /// Binary: Roletypes of interest get an "ANTAG" label
