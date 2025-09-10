c8e777ffc7bc1899385dd2a6ea3cf655cf1a4f9c
nikitosych <174215049+nikitosych@users.noreply.github.com>
2025-09-10 03:06:45 +0200

    changelog

--- Tools/actions_changelog_rss.py
+++ Tools/actions_changelog_rss.py
@@ -35,22 +35,22 @@ CHANGELOG_RSS_KEY = os.environ.get("CHANGELOG_RSS_KEY")
 
 # Change these to suit your server settings
 # https://docs.fabfile.org/en/stable/getting-started.html#run-commands-via-connections-and-run
-SSH_HOST = "status.spacestation14.com"
+SSH_HOST = "central.ss14.pl"
 SSH_USER = "changelog-rss"
 SSH_PORT = 22
 RSS_FILE = "changelog.xml"
 XSL_FILE = "stylesheet.xsl"
 HOST_KEYS = [
-    "AAAAC3NzaC1lZDI1NTE5AAAAIA7eZ2dAJpAR+ZSkZtXiE/JYlTVwzBKDxe0n6npPLBYl"
+    "AAAAC3NzaC1lZDI1NTE5AAAAIPzvoZqhVgf6cfqrTezho5KGaufW8/H1ukgAIyYe6maQ"
 ]
 
 # RSS feed parameters, change these
-FEED_TITLE       = "Funky Station Changelog"
-FEED_LINK        = "https://github.com/funky-station/funky-station/"
-FEED_DESCRIPTION = "Changelog for Funky Station."
-FEED_LANGUAGE    = "en-US"
-FEED_GUID_PREFIX = "funkystation-changelog-"
-FEED_URL         = "https://status.funkystation.org/changelog.xml"
+FEED_TITLE       = "Polonium Changelog"
+FEED_LINK        = "https://github.com/polonium14/polonium-station"
+FEED_DESCRIPTION = "Lista zmian w Polonium Station"
+FEED_LANGUAGE    = "pl-PL"
+FEED_GUID_PREFIX = "ss14-changelog-polonium-"
+FEED_URL         = "https://central.ss14.pl/cl/changelog.xml"
 
 CHANGELOG_FILE = "Resources/Changelog/Changelog.yml"
 
