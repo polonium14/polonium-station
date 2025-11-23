Simple-Station/Einstein-Engines

Morphine (#2198)
# Description

Brings a number of improvements to the surgical experience. Adds
morphine, a new narcotic which prevents screaming (and mood penalties)
from surgery without putting the patient to sleep. This allows for
lengthier operations without either knocking the player out or having
them scream a lot. Also adds analgesic medipens which contain 15u of
morphine (enough for 75 seconds of operation) and 5u of epinephrine.
These are included in the combat medkit, field surgery kit, and filled
CMO webbing. Additionally adds emergency nitrous oxide tanks which spawn
in paramedic lockers to potentially allow for field surgery. Also adds a
proper syringe case, craftable in the medfab, for those who want to
organise their supplies without using cigarette boxes. Finally, this
(re?)enables mood penalties for surgery without morphine/NO2 to
hopefully incentivise medics to interface with the system.

---

<details><summary><h1>Media</h1></summary>
<p>

![Morphine guidebook
entry](https://github.com/user-attachments/assets/2002e4da-da64-4c5a-b25e-36281166bfe5)
![A felinid is painlessly relieved of his
brain](https://github.com/user-attachments/assets/91ec0d32-ddc5-4186-b0fd-ae9b0f6ffea3)


</p>
</details>

---

# Changelog

:cl:
- add: Added morphine
- add: Added syringe case
- add: Added analgesic medipen
- add: Added emergency nitrous oxide tank
- tweak: Added pain pens to advanced medipen kits
- tweak: (Re?)enabled surgery pain moodlet

---------

Signed-off-by: GNUtopia <93669372+GNUtopia@users.noreply.github.com>

Content.Server/_Shitmed/Medical/Surgery/SurgerySystem.cs
@@ -161,7 +161,7 @@ public sealed class SurgerySystem : SharedSurgerySystem
 
     private void OnStepScreamComplete(Entity<SurgeryStepEmoteEffectComponent> ent, ref SurgeryStepEvent args)
     {
-        if (HasComp<ForcedSleepingComponent>(args.Body))
+        if (HasComp<ForcedSleepingComponent>(args.Body) || HasComp<NoScreamComponent>(args.Body))
             return;
 
         _chat.TryEmoteWithChat(args.Body, ent.Comp.Emote);

polonium14/polonium-station

Content.Server/_Shitmed/Medical/Surgery/SurgerySystem.cs
@@ -267,7 +267,7 @@ public sealed class SurgerySystem : SharedSurgerySystem
 
     private void OnStepScreamComplete(Entity<SurgeryStepEmoteEffectComponent> ent, ref SurgeryStepEvent args)
     {
-        if (HasComp<AnesthesiaComponent>(args.Body)) // DeltaV
+        if (HasComp<AnesthesiaComponent>(args.Body) || HasComp<NoScreamComponent>(args.Body))
             return;
 
         _chat.TryEmoteWithChat(args.Body, ent.Comp.Emote);