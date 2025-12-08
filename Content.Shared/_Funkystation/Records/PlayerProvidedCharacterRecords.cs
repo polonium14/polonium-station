c7e5dfed6d554ca221027fb4c4e6e2e9d80120c0
nikitosych <174215049+nikitosych@users.noreply.github.com>
2025-12-08 03:16:05 +0100

    Tłumaczenia akt medycznych

--- Content.Shared/_Funkystation/Records/PlayerProvidedCharacterRecords.cs
+++ Content.Shared/_Funkystation/Records/PlayerProvidedCharacterRecords.cs
@@ -105,7 +105,7 @@ public sealed partial class PlayerProvidedCharacterRecords
             insuranceType: 0,
             medicalInfo: [],
             bloodType: 0,
-            postmortemInstructions: "Return home"
+            postmortemInstructions: "Powrót do domu"
         );
     }
 
