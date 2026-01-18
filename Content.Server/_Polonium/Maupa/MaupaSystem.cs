8e550972f44026bf7e3f77a82f5449322d772d8b
nikitosych <174215049+nikitosych@users.noreply.github.com>
2026-01-18 21:13:45 +0100

    cleanup

--- Content.Server/_Polonium/Maupa/MaupaSystem.cs
+++ Content.Server/_Polonium/Maupa/MaupaSystem.cs
@@ -5,23 +5,12 @@
 
 using Content.Server.Chat;
 using Content.Server.Chat.Systems;
-using Content.Server.NPC.HTN;
-using Content.Server.NPC.Systems;
-using Content.Shared.ActionBlocker;
-using Robust.Shared.Map;
-using Robust.Shared.Timing;
 
 namespace Content.Server._Polonium.Maupa;
 
 public sealed class MaupaSystem : EntitySystem
 {
     [Dependency] private readonly AutoEmoteSystem _autoEmote = default!;
-    [Dependency] private readonly NPCSystem _npc = default!;
-    [Dependency] private readonly IMapManager _mapManager = default!;
-    [Dependency] private readonly IGameTiming _timing = default!;
-    [Dependency] private readonly ActionBlockerSystem _actionBlocker = default!;
-    [Dependency] private readonly ChatSystem _chat = default!;
-
     public override void Initialize()
     {
         base.Initialize();
@@ -33,10 +22,5 @@ public sealed class MaupaSystem : EntitySystem
 
         EnsureComp<AutoEmoteComponent>(uid);
         _autoEmote.AddEmote(uid, "MaupaScream");
-
-        //if (TryComp<HTNComponent>(uid, out var htn))
-        //{
-        //    _npc.WakeNPC(uid, htn);
-        //}
     }
 }
