832cec49341ee30a37b91a40192db32469cdba99
Dusty_Plant <34702649+Dusty-Plant@users.noreply.github.com>
2026-03-30 03:55:32 +0200

    Bramble station (#3061)
    * Update benchmarks.yml
    
    * Add Bramble station for playtesting
    
    * Revert "Update benchmarks.yml"
    
    This reverts commit bd62092fec2fad64bf5914b035e6ee34ed9712f0.
    
    * Update PostMapInitTest.cs
    
    * Revert "Update benchmarks.yml"
    
    This reverts commit bd62092fec2fad64bf5914b035e6ee34ed9712f0.
    
    * small fixes and changes as per feedback
    
    added airlocks separating the medbay to the back of medical
    replaced the engi protolathe with a machine frame
    replaced officer lockers in checkpoints with evidence lockers
    command park airlocks changed from common to engi on the east side and sec on the west side
    added some meteor shielding for the bridge so the mines dont blow it all up when a meteor strikes
    
    went from 5 secoffs and 5 cadets to 7 (max 8 with late joins) secoffs and 3 cadets.
    added the possibility for a second mime to latejoin.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -149,6 +149,7 @@ namespace Content.IntegrationTests.Tests
             "Glacier", // funkystation
 			"Eden", // funkystation
 			"Court", // funkystation
+			"Bramble", // funkystation
             "Barratry", // Harmony
             "Eclipse", // Harmony
             "Kettle", // Goobstation - add Kettle
