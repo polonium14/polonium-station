2da742535eccc4180a4874bf20b8190a613cc1e9
nikitosych <174215049+nikitosych@users.noreply.github.com>
2026-06-23 09:17:03 +0200

    dostosowanie akcentów do forky

--- Content.Server/Speech/EntitySystems/MalopolskiAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/MalopolskiAccentSystem.cs
@@ -5,6 +5,7 @@
 
 using System.Text.RegularExpressions;
 using Content.Server.Speech.Components;
+using Content.Shared.Speech;
 using Robust.Shared.Random;
 
 namespace Content.Server.Speech.EntitySystems;
