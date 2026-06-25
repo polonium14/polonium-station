0316441b46cf9fbb8e0f931c41e2a52cf61a1163
maciejwalendziuk <15122746+maciejwalendziuk@users.noreply.github.com>
2026-06-25 19:26:53 +0200

    Refactor telefonów (#706)

--- Content.Shared/_Polonium/CallablePhone/SharedCallablePhoneSystem.cs
+++ Content.Shared/_Polonium/CallablePhone/SharedCallablePhoneSystem.cs
@@ -185,21 +185,7 @@ public abstract class SharedCallablePhoneSystem : EntitySystem
         if (source.IsCentComm)
             return true;
 
-        if (source.OnlyCentCommInDirectory)
-            return receiver.IsCentComm && source.IncludeCentCommInDirectory;
-
-        if (receiver.ListedInDirectory)
-        {
-            if (source.ExcludeCentCommFromDial && receiver.IsCentComm)
-                return false;
-
-            return true;
-        }
-
-        if (receiver.IsCentComm && source.IncludeCentCommInDirectory)
-            return true;
-
-        return false;
+        return receiver.PhoneGroups.Overlaps(source.DialableGroups);
     }
 
     /// <summary>
