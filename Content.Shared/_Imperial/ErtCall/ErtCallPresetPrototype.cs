7f7edb52ec63144f3f3fb0134fe96e14ea91e4a5
nikitosych <174215049+nikitosych@users.noreply.github.com>
2026-06-29 23:02:18 +0200

    Port `callert`, wahadłowców JSR, terminal WTH, loadouty JSR

---
+++ Content.Shared/_Imperial/ErtCall/ErtCallPresetPrototype.cs
@@ -0,0 +1,13 @@
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Imperial.ErtCall;
+
+[Prototype("ertCall")]
+public sealed partial class ErtCallPresetPrototype : IPrototype
+{
+    [IdDataField] public string ID { get; private set; } = default!;
+
+    [DataField("path")] public string Path { get; set; } = string.Empty;
+
+    [DataField("desc")] public string Desc { get; set; } = string.Empty;
+}
---
+++ Resources/Locale/en-US/_Imperial/ErtCall/RubberStamp.ftl
@@ -0,0 +1,2 @@
+stamp-component-stamped-name-ert-leader = ERT Leader
+stamp-component-stamped-name-cburn-leader = CBURN Leader
