414cce5054f9b8c89b71ec5ef68e2547b99f5379
MaiaArai <158123176+YaraaraY@users.noreply.github.com>
2026-07-05 16:17:39 -0300

    Duo Emotes (#179)
    * duo emotes
    
    * ok rga validator
    
    * animation system, tail swipe spin, kobolds & monkeys can do emotes

---
+++ Content.Shared/_Funkystation/Emoting/Components/DuoEmoteVisualsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Content.Shared._Funkystation.Emoting.Prototypes;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Funkystation.Emoting.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
+public sealed partial class DuoEmoteVisualsComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public ProtoId<DuoEmotePrototype> EmoteId;
+}
---
+++ Content.Shared/_Funkystation/Emoting/DuoEmoteLungeEvent.cs
@@ -0,0 +1,14 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._Funkystation.Emoting;
+
+/// <summary>
+/// Raised on clients to trigger lunge animations for both participants
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class DuoEmoteLungeEvent(NetEntity initiator, NetEntity partner, string? animation) : EntityEventArgs
+{
+    public NetEntity Initiator { get; } = initiator;
+    public NetEntity Partner { get; } = partner;
+    public string? Animation { get; } = animation;
+}
