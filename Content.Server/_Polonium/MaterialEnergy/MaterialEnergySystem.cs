5bd940c03e7ffd1b611f4425a10884a0af367bbf
Szyszkrzyneczka <52501307+Szyszkrzyneczka@users.noreply.github.com>
2026-07-15 11:55:45 +0200

    [FORKY] działa plazmowe znowu działają i przywrócenie dobrobytu w szafkach górników (#758)
    * Tego brakuje
    
    * niemoge tego przetestować ups
    
    * linter krzyczy
    
    * dodaje partial do klasy chuj wie co to znaczy ale linter drze jape

---
+++ Content.Server/_Polonium/MaterialEnergy/MaterialEnergySystem.cs
@@ -0,0 +1,94 @@
+// SPDX-FileCopyrightText: 2024 Piras314 <p1r4s@proton.me>
+// SPDX-FileCopyrightText: 2024 Tadeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2024 yglop <95057024+yglop@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+// SPDX-FileCopyrightText: 2026 Szyszkrzyneczka <rammus.vult@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later AND MIT
+
+using Content.Shared.Interaction;
+using Content.Shared.Materials;
+using Content.Shared.Stacks;
+using Content.Server.Stack;
+using Content.Server.Power.EntitySystems;
+using Content.Server.Power.Components;
+
+namespace Content.Server._Polonium.MaterialEnergy
+{
+    public sealed partial class MaterialEnergySystem : EntitySystem
+    {
+        [Dependency] private IEntityManager _entityManager = default!;
+        [Dependency] private BatterySystem _batterySystem = default!;
+        [Dependency] private StackSystem _stack = default!;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            SubscribeLocalEvent<MaterialEnergyComponent, InteractUsingEvent>(OnInteract);
+        }
+
+        private void OnInteract(EntityUid uid, MaterialEnergyComponent component, InteractUsingEvent args)
+        {
+            if (component.MaterialWhiteList == null)
+                return;
+
+            _entityManager.TryGetComponent<PhysicalCompositionComponent>(args.Used, out var _composition);
+            if (_composition == null)
+                return;
+
+            _entityManager.TryGetComponent<StackComponent>(args.Used, out var materialStack);
+            if (materialStack == null)
+                return;
+
+            foreach (var fueltype in component.MaterialWhiteList)
+            {
+                if (_composition.MaterialComposition.ContainsKey(fueltype))
+                {
+                    AddBatteryCharge(
+                        uid,
+                        args.Used,
+                        _composition.MaterialComposition[fueltype],
+                        materialStack.Count);
+                    args.Handled = true;
+                }
+            }
+        }
+
+        private void AddBatteryCharge(
+            EntityUid cutter,
+            EntityUid _material,
+            int materialPerSheet,
+            int sheetsInStack)
+        {
+            var chargeDiff = _batterySystem.GetChargeDifference(cutter);
+            if (chargeDiff == 0)
+                return;
+
+            var totalMaterial = materialPerSheet * sheetsInStack;
+            var materialLeft = totalMaterial - chargeDiff;
+            var chargeToAdd = 0;
+
+            if (materialLeft == 0)
+            {
+                chargeToAdd = totalMaterial;
+            }
+            else if (materialLeft > 0)
+            {
+                chargeToAdd = (totalMaterial - materialLeft);
+            }
+            else
+            {
+                chargeToAdd = Math.Abs(Math.Abs(materialLeft) - chargeDiff);
+            }
+
+            _batterySystem.AddCharge(cutter, chargeToAdd);
+
+            var toDel = _stack.Split(
+                (EntityUid) _material,
+                chargeToAdd / materialPerSheet,
+                Transform(_material).Coordinates);
+            QueueDel(toDel);
+        }
+    }
+}
