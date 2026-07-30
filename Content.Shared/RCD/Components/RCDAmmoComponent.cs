0c45f9a6ffcd767ee08e82c8d01779c5e919c333
Czuat <143895262+Czuat@users.noreply.github.com>
2026-07-30 21:27:34 +0200

    Update RCDAmmoComponent.cs (#858)

--- Content.Shared/RCD/Components/RCDAmmoComponent.cs
+++ Content.Shared/RCD/Components/RCDAmmoComponent.cs
@@ -22,5 +22,5 @@ public sealed partial class RCDAmmoComponent : Component
     /// Can be partially transferred into an RCD, until it is empty then it gets deleted.
     /// </summary>
     [DataField("charges"), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
-    public FixedPoint2 Charges = 30;
+    public FixedPoint2 Charges = 300;
 }
