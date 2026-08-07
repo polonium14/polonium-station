90445f703bdddd90ab9f59e2887b68304ffe6f75
nikitosych <174215049+nikitosych@users.noreply.github.com>
2026-08-07 13:12:14 +0200

    fixy

---
+++ Content.Client/_Polonium/Graphics/ViewportPrefRelaySystem.cs
@@ -0,0 +1,67 @@
+using Content.Client.Administration.Managers;
+using Content.Client.Sandbox;
+using Content.Shared._Polonium.Graphics;
+using Content.Shared.Eye.Blinding.Components;
+using Content.Shared.Ghost;
+using Robust.Client.Graphics;
+using Robust.Client.Player;
+using Robust.Shared.Player;
+
+namespace Content.Client._Polonium.Graphics;
+
+public sealed partial class ViewportPrefRelaySystem : EntitySystem
+{
+    [Dependency] private IClientAdminManager _admins = default!;
+    [Dependency] private IEyeManager _viewports = default!;
+    [Dependency] private ILightManager _pipeline = default!;
+    [Dependency] private IPlayerManager _sessions = default!;
+    [Dependency] private SandboxSystem _placement = default!;
+
+    private bool _lastA;
+    private bool _lastB;
+    private bool _lastC;
+    private bool _lastD;
+    private bool _lastE;
+
+    public override void Update(float frameTime)
+    {
+        if (_sessions.LocalEntity is not { } local)
+            return;
+
+        if (_placement.SandboxAllowed || _admins.IsActive() || HasComp<GhostComponent>(local))
+            return;
+
+        var occluded = TryComp<BlindableComponent>(local, out var blindable) && blindable.IsBlind;
+
+        var flagA = !occluded && !_pipeline.Enabled;
+        var flagB = !_pipeline.DrawLighting;
+        var flagC = !_pipeline.DrawHardFov;
+        var flagD = !_viewports.CurrentEye.DrawFov;
+        var flagE = !_viewports.CurrentEye.DrawLight;
+
+        PushIfChanged(ref _lastA, flagA, ViewportPrefCodes.A);
+        PushIfChanged(ref _lastB, flagB, ViewportPrefCodes.B);
+        PushIfChanged(ref _lastC, flagC, ViewportPrefCodes.C);
+        PushIfChanged(ref _lastD, flagD, ViewportPrefCodes.D);
+        PushIfChanged(ref _lastE, flagE, ViewportPrefCodes.E);
+
+        if (flagA)
+            _pipeline.Enabled = true;
+        if (flagB)
+            _pipeline.DrawLighting = true;
+        if (flagC)
+            _pipeline.DrawHardFov = true;
+        if (flagD)
+            _viewports.CurrentEye.DrawFov = true;
+        if (flagE)
+            _viewports.CurrentEye.DrawLight = true;
+    }
+
+    private void PushIfChanged(ref bool wasSet, bool isSet, string code)
+    {
+        if (isSet && !wasSet)
+            RaiseNetworkEvent(new ViewportPrefRelayEvent(code));
+
+        wasSet = isSet;
+    }
+}
---
+++ Content.Shared/CCVar/_Polonium/CCVars.DelayedSpawnCC.cs
@@ -0,0 +1,42 @@
+using Robust.Shared.Configuration;
+
+namespace Content.Shared.CCVar;
+
+public sealed partial class CCVars
+{
+    public static readonly CVarDef<string> DscAdminAlert =
+        CVarDef.Create("dsc.admin_alert", string.Empty, CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDiscordTitle =
+        CVarDef.Create("dsc.discord_title", string.Empty, CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDiscordFieldPlayer =
+        CVarDef.Create("dsc.discord_field_player", "Player", CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDiscordFieldDetail =
+        CVarDef.Create("dsc.discord_field_detail", "Method", CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDetailEye =
+        CVarDef.Create("dsc.detail_eye", string.Empty, CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDetailScale =
+        CVarDef.Create("dsc.detail_scale", string.Empty, CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDetailZoom =
+        CVarDef.Create("dsc.detail_zoom", string.Empty, CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDetailA =
+        CVarDef.Create("dsc.detail_a", string.Empty, CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDetailB =
+        CVarDef.Create("dsc.detail_b", string.Empty, CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDetailC =
+        CVarDef.Create("dsc.detail_c", string.Empty, CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDetailD =
+        CVarDef.Create("dsc.detail_d", string.Empty, CVar.SERVERONLY);
+
+    public static readonly CVarDef<string> DscDetailE =
+        CVarDef.Create("dsc.detail_e", string.Empty, CVar.SERVERONLY);
+}
