96ef3c9b783b32700ccd0950e438091ab5db068d
Nikita (Nick) <174215049+nikitosych@users.noreply.github.com>
2026-09-16 09:45:24 +0200

    Update actions_changelogs_since_last_run.py [ci skip]

--- Tools/actions_changelogs_since_last_run.py
+++ Tools/actions_changelogs_since_last_run.py
@@ -231,7 +231,7 @@ def changelog_entries_to_message_lines(entries: Iterable[ChangelogEntry]) -> lis
 
     for contributor_name, group in itertools.groupby(entries, lambda x: x["author"]):
         message_lines.append("\n")
-        message_lines.append(f"**{contributor_name}** Zaktualizował(-a):\n")
+        message_lines.append(f"**{contributor_name}** zaktualizował(-a):\n")
 
         for entry in group:
             url = entry.get("url")
