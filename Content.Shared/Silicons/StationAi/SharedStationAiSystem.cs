ab1640f78c14d16a634a0f30f1032b2ced1cfd50
nikitosych <174215049+nikitosych@users.noreply.github.com>
2026-09-17 12:11:41 +0200

    Revert firelocków

--- Content.Shared/Silicons/StationAi/SharedStationAiSystem.cs
+++ Content.Shared/Silicons/StationAi/SharedStationAiSystem.cs
@@ -1,4 +1,3 @@
-using Content.Shared._Funkystation.FirelockBolt.EntitySystems;
 using Content.Shared.Access.Systems;
 using Content.Shared.ActionBlocker;
 using Content.Shared.Actions;
@@ -54,7 +53,6 @@ public abstract partial class SharedStationAiSystem : EntitySystem
     [Dependency] private SharedDoorSystem _doors = default!;
     [Dependency] private SharedDoAfterSystem _doAfter = default!;
     [Dependency] private SharedElectrocutionSystem _electrify = default!;
-    [Dependency] private SharedFirelockBoltControlSystem _firelockBolts = default!;
     [Dependency] private SharedEyeSystem _eye = default!;
     [Dependency] protected SharedMapSystem Maps = default!;
     [Dependency] private SharedMindSystem _mind = default!;
--- Resources/Locale/pl-PL/_Funkystation/doors/firelocks.ftl
+++
@@ -1,8 +0,0 @@
-firelock-bolt-control-window-title = Bolt Override
-firelock-bolt-control-window-status-locked = ZABOLTOWANE
-firelock-bolt-control-window-status-unlocked = ODBLOKOWANE
-firelock-bolt-control-window-status-override = NADPISANIE AKTYWNE
-firelock-bolt-control-window-hint = Pociągnij, aby nadpisać bolce.
-firelock-bolt-control-verb-open-ui = Otwórz panel nadpisania bolców
-firelock-bolt-control-remote-override = Ręczne nadpisanie jest aktywne - sterowanie zdalne wyłączone
-firelock-bolt-control-remote-hazard = Śluza przeciwpożarowa izoluje zagrożenie - bolce nie mogą zostać zwolnione
