435277800c8567095391cc260da9a62c661f5ba5
Nikita (Nick) <174215049+nikitosych@users.noreply.github.com>
2026-09-22 16:26:24 +0200

    Predykcja strzał piankowych, strzał i podobnych pocisków (#1047)
    * jebannananannananana kurwa predykcja
    
    * fix toy sprite
    
    * proper impact write

--- Content.Shared/_RMC14/Weapons/Ranged/Prediction/PredictedProjectileHitEvent.cs
+++ Content.Shared/_RMC14/Weapons/Ranged/Prediction/PredictedProjectileHitEvent.cs
@@ -4,8 +4,14 @@ using Robust.Shared.Serialization;
 namespace Content.Shared._RMC14.Weapons.Ranged.Prediction;
 
 [Serializable, NetSerializable]
-public sealed class PredictedProjectileHitEvent(int projectile, HashSet<(NetEntity Id, MapCoordinates Coordinates)> hit) : EntityEventArgs
+public sealed class PredictedProjectileHitEvent(
+    int projectile,
+    HashSet<(NetEntity Id, MapCoordinates Coordinates)> hit,
+    MapCoordinates impact) : EntityEventArgs
 {
     public readonly int Projectile = projectile;
     public readonly HashSet<(NetEntity Id, MapCoordinates Coordinates)> Hit = hit;
+
+    // where athe shooter already saw the projectile stop
+    public readonly MapCoordinates Impact = impact;
 }
